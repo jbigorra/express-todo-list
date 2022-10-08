d2ffca2f08b6175bfa9f9a712c4076f3a96832f7
Juan bigorra <juanchoibf@gmail.com>
2022-10-08 22:12:42 +0200

    Create users and show them with server-side rendering

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
+const users = [
+    { username: "Pepito" },
+    { username: "Manolito" },
+];
+
+const usersRepo = {
+    users: [
+        { username: "Pepito" },
+        { username: "Manolito" },
+    ]
+};
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json({payload: []});
+
+  res.render("users/index", { users });
+});
+
+router.post('/', function(req, res, next) {
+    const user = {
+        username: req.body.username
+    };
+
+    users.push(user);
+
+    console.log(users)
+
+    res.render("users/index", { users });
 });
 
 module.exports = router;
